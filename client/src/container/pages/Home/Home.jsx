350669d65d96485e78aae44eb3baddd4ceccac5e
SniperDev716 <sniper.dev716@gmail.com>
2024-03-27 13:40:05 -0700

    fix category img

--- client/src/container/pages/Home/Home.jsx
+++ client/src/container/pages/Home/Home.jsx
@@ -9,22 +9,24 @@ const { Content, Sider } = Layout;
 const { Title, Text } = Typography;
 
 const categories = [
-  { src: "", title: "Product Renderings" },
-  { src: "", title: "Package Design" },
-  { src: "", title: "Logo Design" },
-  { src: "", title: "Resturant Design" },
-  { src: "", title: "Room Design" },
-  { src: "", title: "Store Design" },
-  { src: "", title: "Advertisement Design" },
-  { src: "", title: "App Design" },
-  { src: "", title: "Website Design" },
-  { src: "", title: "Flyer Design" },
-  { src: "", title: "Business Cards" },
-  { src: "", title: "Album Art" },
-  { src: "", title: "Stock Photos" },
-  { src: "", title: "Charts & Graphs" },
-  { src: "", title: "Clothing Design" },
-  { src: "", title: "Food Photography" },
+  { src: "(1) Product Design.png", title: "Product Design" },
+  { src: "(2) Packaging.png", title: "Package Design" },
+  { src: "(3) Logo design.png", title: "Logo Design" },
+  { src: "(4) Resturant Design.png", title: "Resturant Design" },
+  { src: "(5) Home Design.png", title: "Room Design" },
+  { src: "(6) Store Design.png", title: "Store Design" },
+  { src: "(7) Advertisement.png", title: "Advertisement Design" },
+  { src: "(8) App Design.png", title: "App Design" },
+  { src: "(9) Website Design.png", title: "Website Design" },
+  { src: "(10) Flyer Design.png", title: "Flyer Design" },
+  { src: "(11) Business Cards.png", title: "Business Cards" },
+  { src: "(12) Sticker Design.png", title: "Sticker Design" },
+  { src: "(13) Album Art.png", title: "Album Art" },
+  { src: "(14) Signage.png", title: "Signage Design" },
+  { src: "(15) Automotive.png", title: "Automotive Design" },
+  { src: "(16) Fashion.png", title: "Fashion Design" },
+  { src: "(17) Furniture.png", title: "Furniture Design" },
+  { src: "(18) Film Concept Art.png", title: "Film Concept Art" },
 ]
 
 function Home() {
@@ -50,17 +52,17 @@ function Home() {
   }
 
   return (
-    <Content className="text-center max-w-5xl mx-auto p-2">
+    <Content className="text-center max-w-5xl mx-auto p-2 my-5">
       <Row>
         <Col span={24}>
-          <Title>We'll walk you through the first, and then let you explore your creativity. Pick a category to start with.</Title>
+          <Title level={3}>We'll walk you through the first, and then let you explore your creativity. Pick a category to start with.</Title>
         </Col>
         <Col span={24}>
           <Row gutter={[32, 32]}>
             {categories.map((cat, index) => <Col key={index} span={8}>
               <div className="text-center">
-                <div className="w-40 h-40 mx-auto bg-gray-300 cursor-pointer rounded shadow-sm hover:shadow-lg" onClick={() => handleNext(cat)}></div>
-                <Text>{cat.title}</Text>
+                <img src={`/imgs/category/${cat.src}`} className="w-[80%] mx-auto bg-gray-300 cursor-pointer rounded shadow-sm hover:shadow-lg" onClick={() => handleNext(cat)}></img> <br />
+                <Text className="text-xl font-bold">{cat.title}</Text>
               </div>
             </Col>)}
           </Row>
