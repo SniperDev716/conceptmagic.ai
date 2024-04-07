0b0caf05e56ea1e5a4ba43e0190898d116855c63
SniperDev716 <sniper.dev716@gmail.com>
2024-04-07 10:53:33 -0700

    fix

--- client/src/container/layouts/AuthLayout.jsx
+++ client/src/container/layouts/AuthLayout.jsx
@@ -47,55 +47,57 @@ function AuthLayout({ children }) {
   }
 
   return (
-    <Layout className="min-h-screen">
-      <Header className={classNames("shadow sticky px-0 top-0 z-[999]", !isDarkMode && "bg-white")}>
-        <div className="flex items-center justify-between px-2 max-w-7xl mx-auto">
-          <div className={classNames("demo-logo h-[64px] mb-2", !isDarkMode && "bg-white")}>
-            <Link to="/projects" className="hidden sm:inline">
-              <img src={LogoSrc} alt="logo" className="w-[64px] p-3" />
-            </Link>
-            <Link to="/projects" className="inline sm:hidden">
-              <img src={smLogoSrc} alt="logo" className="w-[64px] p-3" />
-            </Link>
-          </div>
-          <div className="flex items-center">
-            <div className="mr-2 sm:mr-6"><Button type="" onClick={changeTheme} shape="circle" icon={<MoonFilled />}></Button></div>
-            <div>
-              <Link to="/projects" className="mr-2 sm:mr-6">
-                My Projects
+    <>
+      <Layout className="min-h-screen">
+        <Header className={classNames("shadow sticky px-0 top-0 z-[999]", !isDarkMode && "bg-white")}>
+          <div className="flex items-center justify-between px-2 max-w-7xl mx-auto">
+            <div className={classNames("demo-logo h-[64px] mb-2", !isDarkMode && "bg-white")}>
+              <Link to="/projects" className="hidden sm:inline">
+                <img src={LogoSrc} alt="logo" className="w-[64px] p-3" />
+              </Link>
+              <Link to="/projects" className="inline sm:hidden">
+                <img src={smLogoSrc} alt="logo" className="w-[64px] p-3" />
               </Link>
             </div>
-            <div className="mr-2 sm:mr-6">
-              <Button type="primary" icon={<FileAddOutlined />} onClick={() => {
-                return navigate('/home');
-                // if (user.activeSubscription) {
-                // }
-                // setOpenModal();
-              }}>New Project</Button>
+            <div className="flex items-center">
+              <div className="mr-2 sm:mr-6"><Button type="" onClick={changeTheme} shape="circle" icon={<MoonFilled />}></Button></div>
+              <div>
+                <Link to="/projects" className="mr-2 sm:mr-6">
+                  My Projects
+                </Link>
+              </div>
+              <div className="mr-2 sm:mr-6">
+                <Button type="primary" icon={<FileAddOutlined />} onClick={() => {
+                  return navigate('/home');
+                  // if (user.activeSubscription) {
+                  // }
+                  // setOpenModal();
+                }}>New Project</Button>
+              </div>
+              <UserMenu />
             </div>
-            <UserMenu />
           </div>
+        </Header>
+        <div onClick={() => {
+          navigate(-1);
+        }} className={classNames("sticky w-[75px] cursor-pointer top-20 -ml-11 hover:ml-0 ease-in-out transition-[margin] z-[998] px-2.5 py-2 rounded-e-3xl", isDarkMode ? "bg-gray-100" : "bg-gray-300")}>
+          <span>Back</span> &nbsp; <ArrowLeftOutlined />
         </div>
-      </Header>
-      <Layout>
-        {children}
+        <Layout>
+          {children}
+        </Layout>
+        <ScrollToTop smooth className="animate-bounce" />
+        <Elements stripe={stripePromise} nonce="random-nonce">
+          <PayModal
+            open={openPayModal}
+            setOpen={setOpenModal}
+            price={plans[0]?.price}
+            planId={plans[0]?._id}
+            setSuccessful={() => { }}
+          />
+        </Elements>
       </Layout>
-      <ScrollToTop smooth className="animate-bounce" />
-      <Elements stripe={stripePromise} nonce="random-nonce">
-        <PayModal
-          open={openPayModal}
-          setOpen={setOpenModal}
-          price={plans[0]?.price}
-          planId={plans[0]?._id}
-          setSuccessful={() => { }}
-        />
-      </Elements>
-      <div onClick={() => {
-        navigate(-1);
-      }} className={classNames("absolute cursor-pointer top-20 -left-11 hover:left-0 ease-in-out transition-[left] z-[9999] px-2.5 py-2 rounded-e-3xl", isDarkMode ? "bg-gray-100" : "bg-gray-300")}>
-        <span>Back</span> &nbsp; <ArrowLeftOutlined />
-      </div>
-    </Layout>
+    </>
   );
 }
 
